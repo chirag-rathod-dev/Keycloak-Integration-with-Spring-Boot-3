0d147a1526ee0d934ff659504e4db2ad0427545c
chirag-rathod-dev <chirag.rathod.dev@gmail.com>
2025-03-04 10:28:59 +0530

    Add Project files

--- src/test/java/com/rathod/Keycloak/KeycloakIntegrationWithSpringBoot3ApplicationTests.java
+++ src/test/java/com/rathod/Keycloak/KeycloakIntegrationWithSpringBoot3ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KeycloakIntegrationWithSpringBoot3ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
